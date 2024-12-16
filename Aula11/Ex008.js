22ae9e965ef119588cf216288c015cdbb00140d0
Leonardo Gibertoni <gibertonileo@gmail.com>
2024-12-16 12:59:49 -0300

    Novos Exercicios

---
+++ Aula11/Ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 54
+console.log(`A velocidade do seu carro é: ${velocidade}Km/h`)
+
+if (velocidade >= 60){
+    console.log("Você passou do limite de Velocidade. Multado!!")
+}
+
+console.log("Ande sempre com sinto de segurança!")
---
+++ Aula11/Ex009.js
@@ -0,0 +1,12 @@
+var pais = 'Japan'
+console.log(`Vivendo em ${pais}`)
+
+if (pais == 'Brasil'){
+    console.log("Vocé é Brasileiro!!")
+}
+else if(pais == 'Japan'){
+    console.log("Vocé é Japones!!")
+}
+else{
+    console.log("Vocé é Estrangeiro!")
+}
