2bc491c3bf9221661d615442b087bee80e1f2c6a
Leonardo Gibertoni <gibertonileo@gmail.com>
2024-12-16 13:39:45 -0300

    Atualização Exercicios

---
+++ Aula12/Ex011.js
@@ -0,0 +1,13 @@
+var idade = 65
+if (idade < 16){
+    console.log('Não pode votar')
+}
+else if (idade<18){
+    console.log('Voto Opcional')
+}
+else if (idade >= 18 && idade < 65){
+    console.log('Pode votar')
+}
+else{
+    console.log('Voto Opcional')
+}
