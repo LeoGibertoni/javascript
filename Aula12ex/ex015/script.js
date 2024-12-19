6753e92f3b4a906a4b8a119d211f7a782effe311
Leonardo Gibertoni <gibertonileo@gmail.com>
2024-12-19 12:11:03 -0300

    Novas Atividades

---
+++ Aula12ex/ex015/script.js
@@ -0,0 +1,20 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'homem'
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+        }
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+    }
+}
